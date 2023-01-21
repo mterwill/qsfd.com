4c618500431017fffde84fbf34afb9f8b73073dc
bdtyler <34323989+bdtyler@users.noreply.github.com>
2023-01-21 07:08:26 -0500

    Update random_image.js

--- assets/js/random_image.js
+++ assets/js/random_image.js
@@ -1,11 +1,14 @@
 $(function() {
   var images = [
+    'atthescene.jpg',	  
     'temp-truck.jpg',
     'stations-01.jpg',
     'stations-02.jpg',
+    'divers.jpg',	  
     'eta-497.jpg',
     'er-493.jpg',
     'two-trucks.jpg',
+    'fundraising.jpg',	  
     'IMG_0147.jpg',
     'IMG_0191.jpg',
     'IMG_8583.jpg',
